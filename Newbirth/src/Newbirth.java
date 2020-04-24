810a93a5d81de88ce8e7dd3ff498c8ede1818eae
HNbicycle <710885333@qq.com>
2020-04-24 13:35:34 +0800

    NewBirth

---
+++ Newbirth/src/Newbirth.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Newbirth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Newbirth!yeah!");
+//		Scanner in=new Scanner(System.in);
+//		System.out.println("Bill:" + in.nextLine());
+	}
+
+}
